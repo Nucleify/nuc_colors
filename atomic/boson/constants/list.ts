944c54c651242ca092f9d70c44ac22a0a167a28a
SzymCode <szymon.radomski@yahoo.com>
2026-01-11 16:30:48 +0100

    refactor(store): lazy-init color groups and list

--- atomic/boson/constants/list.ts
+++ atomic/boson/constants/list.ts
@@ -1,18 +1,25 @@
 import type { SettingsGroupInterface } from 'atomic'
-import { modulesGroups } from 'atomic'
 
-export const colorGroups: SettingsGroupInterface[] =
-  typeof window !== 'undefined' && typeof modulesGroups === 'function'
-    ? [
-        {
-          name: 'Main',
-          items: ['Main'],
-        },
-        ...modulesGroups(),
-      ]
-    : []
+import { modulesGroups } from '../../../../nuc_settings/constants/modules'
 
-export const colorList: string[] =
-  typeof window !== 'undefined'
-    ? [...colorGroups.flatMap((group) => group.items as string[])]
-    : []
+export function getColorGroups(): SettingsGroupInterface[] {
+  return [
+    {
+      name: 'Main',
+      items: ['Main'],
+    },
+    ...modulesGroups(),
+  ]
+}
+
+export function getColorList(): string[] {
+  return getColorGroups().flatMap((group) => group.items as string[])
+}
+
+export const colorGroups: SettingsGroupInterface[] = []
+export const colorList: string[] = []
+
+if (typeof window !== 'undefined') {
+  colorGroups.push(...getColorGroups())
+  colorList.push(...getColorList())
+}
